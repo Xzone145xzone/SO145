e51a748e2bef60c040cb4abc2a7ff32d439f00c8
Sox145 <xzone145xzone@gmail.com>
2019-07-01 13:56:50 +0700

    Create Authen

---
+++ android/app/src/main/java/th/ac/ssru/ung_so145/MainActivity.java
@@ -0,0 +1,13 @@
+package th.ac.ssru.ung_so145;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/screens/authen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+main(){}
+
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Authen');
+  }
+}
