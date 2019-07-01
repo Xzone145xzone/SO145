8ef4e0cb755a4cc66cb4358a79ef56dfb2353ac9
Sox145 <xzone145xzone@gmail.com>
2019-07-01 16:26:58 +0700

    finished 1 july

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -7,15 +7,30 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+//Explict
+  double mySize = 180.0;
+
+//Medthod
   Widget showLogo() {
-    return Image.asset(
-      'images/logo.png',
-      fit: BoxFit.contain,
+    return Container(
+      width: mySize,
+      height: mySize,
+      child: Image.asset(
+        'images/logo.png',
+        fit: BoxFit.contain,
+      ),
     );
   }
 
   Widget showText() {
-    return Text('Authen');
+    return Text(
+      'Ung SOxPOP',
+      style: TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.orange[700],fontFamily: 'Inconsolata'
+      ),
+    );
   }
 
   @override
