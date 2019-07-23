9b844ca9b8996f195d00f7efb67f168646bee99b
Sox145 <xzone145xzone@gmail.com>
2019-07-23 11:48:42 +0700

    Register Success

--- lib/screens/myservice.dart
+++ lib/screens/myservice.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 
 class Myservice extends StatefulWidget {
   @override
@@ -9,12 +10,32 @@ class Myservice extends StatefulWidget {
 
 class _MyserviceState extends State<Myservice> {
 // Explicit
-
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  String nameString = '';
 // medthod
+  @override
+  void initState() {
+    super.initState();
+    findDisplayName();
+  }
+
+  Future<void> findDisplayName() async {
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    setState(() {
+      nameString = firebaseUser.displayName;
+    });
+    print('name = $nameString');
+  }
 
   Widget headMenu() {
     return DrawerHeader(
-      decoration: BoxDecoration(),
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          colors: [Colors.yellow, Colors.orange[100], Colors.white],
+          radius: 1.0,
+          center: Alignment.center,
+        ),
+      ),
       child: Column(
         children: <Widget>[
           Container(
@@ -28,7 +49,8 @@ class _MyserviceState extends State<Myservice> {
               color: Colors.orange[800],
               fontSize: 24.0,
             ),
-          ),Text('Login by ...')
+          ),
+          Text('Login by $nameString')
         ],
       ),
     );
@@ -37,10 +59,38 @@ class _MyserviceState extends State<Myservice> {
   Widget showDrewerMenu() {
     return Drawer(
       child: ListView(
-        children: <Widget>[headMenu()],
+        children: <Widget>[
+          headMenu(),
+          signOutAnExit(),
+        ],
       ),
     );
   }
+
+  Widget signOutAnExit() {
+    return ListTile(
+      leading: Icon(
+        Icons.exit_to_app,
+        size: 36.0,
+        color: Colors.red,
+      ),
+      title: Text(
+        'SingOut & Exit',
+        style: TextStyle(
+          fontSize: 18.0,
+        ),
+      ),onTap:(){
+        mySignOut();
+      },
+    );
+  }
+
+Future<void> mySignOut()async{
+await firebaseAuth.signOut().then((response){
+  exit(0);
+});
+}
+
 
   @override
   Widget build(BuildContext context) {
